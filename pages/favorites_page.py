4dbe2094bb83ec52b3dff1908d08ef496d3f2836
Sapronov <sapronov.ov@lime-shop.com>
2024-03-21 17:29:13 +0700

    *

--- pages/favorites_page.py
+++ pages/favorites_page.py
@@ -1,10 +1,19 @@
+import time
 import allure
+import pytest
+
 from pages.base_page import BasePage
+from locators import *
+
 
 
 class FavoritesPage(BasePage):
-    @allure.step('Шаблончик')
-    def print_test(self):
-        print('test')
 
-
+    def open_favorites(self):
+        self.click(MainLocators.FAVORITES_NAV)
+        self.wait_element(FavoritesLocators.TITLE)
+        self.wait_element(FavoritesLocators.BOTTOMBUY)
+        self.wait_text("НАЧАТЬ ПОКУПКИ")
+        self.wait_text("ВАШ ВИШЛИСТ ПУСТ")
+        # button_text = self.get_text(FavoritesLocators.BOTTOMBUY)
+        # assert button_text == "НАЧАТЬ ПОКУПКИ123", print(f"Текст отличается от макета. Текст в приложении {button_text}, а мы ожидали 'НАЧАТЬ ПОКУПКИ123'")
