95a5556097df98e0a351e6399c4b90930ba106ee
Sapronov <sapronov.ov@lime-shop.com>
2024-04-01 09:31:18 +0700

    *

--- pages/favorites_page.py
+++ pages/favorites_page.py
@@ -9,11 +9,6 @@ from locators import *
 
 class FavoritesPage(BasePage):
 
-    def open_favorites(self):
-        self.click(MainLocators.FAVORITES_NAV)
-        self.wait_element(FavoritesLocators.TITLE)
-        self.wait_element(FavoritesLocators.BOTTOMBUY)
-        self.wait_text("НАЧАТЬ ПОКУПКИ")
-        self.wait_text("ВАШ ВИШЛИСТ ПУСТ")
-        # button_text = self.get_text(FavoritesLocators.BOTTOMBUY)
-        # assert button_text == "НАЧАТЬ ПОКУПКИ123", print(f"Текст отличается от макета. Текст в приложении {button_text}, а мы ожидали 'НАЧАТЬ ПОКУПКИ123'")
+
+
+
